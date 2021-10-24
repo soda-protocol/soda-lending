dccee9609a8c9221e5c1d851bbd073c25b859b0d
Fil Fil <gyl19930522@163.com>
2021-10-24 16:41:24 +0800

    update program id

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -13,4 +13,4 @@ pub mod state;
 // Export current sdk types for downstream users building with a different sdk version
 pub use solana_program;
 
-solana_program::declare_id!("Soda18RfA56ECuLJKRqfZgKoCjZDxoMC3n98pdQDbWq");
+solana_program::declare_id!("Soda111Jv27so2PRBd6ofRptC6dKxosdN5ByFhCcR3V");
