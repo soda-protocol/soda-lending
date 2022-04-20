c0b437012e04ba892608bce231964186e6dbc0b6
Lone <gyl19930522@163.com>
2022-04-20 21:35:12 +0800

    update

--- lending/src/oracle/dex_oracle.rs
+++ lending/src/oracle/dex_oracle.rs
@@ -1,10 +1,8 @@
-use solana_program::clock::Slot;
+// use solana_program::clock::Slot;
 
-use crate::math::Decimal;
+// use crate::math::Decimal;
 
-
-
-pub struct DexOracle {
-    pub last_slot: Slot,
-    pub price: Option<Decimal>,
-}
+// pub struct DexOracle {
+//     pub last_slot: Slot,
+//     pub price: Option<Decimal>,
+// }
